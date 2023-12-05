10295caa48546b2f89002c999189d7fe6204bbb7
Nero0017 <67811893+Nero0017@users.noreply.github.com>
2023-12-05 19:28:06 +0800

    Update __init__.py

--- release_tools/__init__.py
+++ release_tools/__init__.py
@@ -1,13 +1,20 @@
 import sys, os, subprocess, pkg_resources
 
 package_name = "tpu_mlir"
-
+package_dependence_name = "tpu_mlir_dependence"
 # get tpu_mlir package path
 try:
     distribution = pkg_resources.get_distribution(package_name)
     package_path = distribution.location + f"/{package_name}"
 except pkg_resources.DistributionNotFound:
     print(f"Package '{package_name}' is not installed. Exiting.")
+    exit()
+    
+try:
+    distribution = pkg_resources.get_distribution(package_dependence_name)
+    package_dependence_path = distribution.location + f"/{package_dependence_name}"
+except pkg_resources.DistributionNotFound:
+    print(f"Package '{package_dependence_name}' is not installed. Exiting.")
     exit()
 
 # set execute permissions
@@ -28,21 +35,18 @@ new_path = [
 ]
 os.environ["PATH"] += "".join(new_path)
 os.environ["PYTHONPATH"] = (
-    f"{package_path}/:"
-    + f"{package_path}/python/:"
-    + f"{package_path}/regression/:"
+    f"{package_path}/:" + f"{package_path}/python/:" + f"{package_path}/regression/:"
+)
+os.environ["LD_LIBRARY_PATH"] = (
+    f"{package_path}/lib:" + f"{package_dependence_path}/lib/third_party:"
 )
 os.environ["OMP_NUM_THREADS"] = "4"
 os.environ["TPUC_ROOT"] = f"{package_path}"
 
-# # This is no longer needer because the rpath of .so files have been changed by patchelf tool in release_pip.sh
-# os.environ["LD_LIBRARY_PATH"] = (
-#     f"{package_path}/lib/third_party:" + f"{package_path}/lib"
-# )
-
 sys.path.append(f"{package_path}/")
 sys.path.append(f"{package_path}/python/")
 sys.path.append(f"{package_path}/regression/")
+
 
 def run_subprocess_py(file_name):
     arguments = sys.argv[1:]
@@ -70,10 +74,10 @@ def run_subprocess_c(file_name):
 
 def cp_from_package_root():
     arguments = sys.argv[1:]
-    if len(arguments)!=2:
+    if len(arguments) != 2:
         print("Both src_path and target_path are and only required!")
         exit(1)
-    command = ["cp","-rf"] + [f"{package_path}/" + arguments[0]] + [arguments[1]]
+    command = ["cp", "-rf"] + [f"{package_path}/" + arguments[0]] + [arguments[1]]
     process = subprocess.Popen(command)
     return_code = process.wait()
     if return_code != 0:
